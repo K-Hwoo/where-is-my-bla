39a7920fee80db1c0bcbb799c44ea763459d0a2c
김현우 <aq3480@naver.com>
2024-03-29 15:51:27 +0900

    멀티미디어 응용 프로젝트 첫 커밋

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+const path = require("path");
+const wimpRoutes = require("./routes/wimp.js");
+
 const express = require("express");
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(wimpRoutes);
+
+app.use(function (error, req, res, next) {
+  console.log(error);
+  res.status(500).render("500");
+});
+
+app.listen(3000);
